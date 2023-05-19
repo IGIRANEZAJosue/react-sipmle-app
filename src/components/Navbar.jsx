ad9d21508449bddfcdf4eac277cab087313a737c
IGIRANEZA Josue <igiranezaj28@gmail.com>
2023-05-19 12:19:23 +0300

    ...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,11 +39,11 @@ const Navbar = () => {
          </div>
 
          <div onClick={handleNav} className="block md:hidden duration-500">
-            { !nav? <i class="text-[24px] fa-solid fa-xmark"></i> : <i class=" text-[24px] fa-solid fa-bars"></i> }
+            { nav? <i class="text-[24px] fa-solid fa-xmark"></i> : <i class=" text-[24px] fa-solid fa-bars"></i> }
          </div>
 
 
-         <div className={!nav ? " md:hidden fixed left-0 top-0 h-full border-r border-r-gray-700 bg-[#313131] text-white z-20 ease-in-out duration-500" : "fixed left-[-100%] md:hidden"}>
+         <div className={nav ? " md:hidden fixed left-0 top-0 h-full border-r border-r-gray-700 bg-[#313131] text-white z-20 ease-in-out duration-500" : "fixed left-[-100%] md:hidden"}>
             
             <div className="logo ml-8 mr-6 mt-8">
                <Link className="flex items-center gap-4 z-40" to={"/"}>
