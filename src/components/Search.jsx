1b31f4ee7137f53daef77772a3352df6c9936f42
IGIRANEZA Josue <igiranezaj28@gmail.com>
2023-06-23 09:10:17 +0300

    search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -15,7 +15,7 @@ const Search = () => {
    return (
       <form onSubmit={submitHandler} className=" form my-2 lg:mx-[8rem] ">
          <div className="flex gap-4">
-            <i className="fa-solid fa-magnifying-glass"></i>
+            <button type="submit"><i className="fa-solid fa-magnifying-glass pl-2"></i></button>
             <input onChange={(e) => setInput(e.target.value)} className="searchInput w-full" type="text" value={input} />
          </div>
       </form>
